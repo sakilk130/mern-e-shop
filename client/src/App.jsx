09ac97e6edd91d26b282143051694fa2ad5bbb79
Sakil Khan <sakilk130@gmail.com>
2022-01-26 15:50:58 +0600

    feat(client): create route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+//components
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+
+//pages
 import Home from './pages/Home';
+import Product from './pages/Product';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <Home />
+          <Route path="/" component={Home} exact />
+          <Route path="/product/:id" component={Product} exact />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
--- client/src/components/Product/Product.jsx
+++ client/src/components/Product/Product.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
 import Rating from '../Rating/Rating';
 
 const Product = ({ product }) => {
   return (
     <Card className="my-3 rounded">
-      <a href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant="top" />
-      </a>
+      </Link>
       <Card.Body>
-        <a href={`/product/${product._id}`} style={{ textDecoration: 'none' }}>
+        <Link to={`/product/${product._id}`} style={{ textDecoration: 'none' }}>
           <Card.Title as="div">
             <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
         <Card.Text as="div">
           <div className="my-3">
             <Rating
