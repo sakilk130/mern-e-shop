fa8fcce3f9691d4289f6f5b652b3936b6b75a275
Sakil Khan <sakilk130@gmail.com>
2022-01-27 11:59:42 +0600

    feat(api): custom error handler

--- api/server.js
+++ api/server.js
@@ -3,7 +3,8 @@ import dotenv from 'dotenv';
 import colors from 'colors';
 
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import productRoutes from './routes/product.routes.js';
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 connectDB();
@@ -15,14 +16,10 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(
   PORT,
