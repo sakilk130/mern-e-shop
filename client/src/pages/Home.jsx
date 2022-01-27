6bd830326f30dd7d6c35188342dd8d9e4cc4b962
Sakil Khan <sakilk130@gmail.com>
2022-01-27 16:59:51 +0600

    feat(client): create loader, message & product details global state

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Col, Row } from 'react-bootstrap';
 import { listProducts } from '../actions/productActions';
 import Product from '../components/Product/Product';
+import Loader from '../components/Loader/Loader';
+import Message from '../components/Message/Message';
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -17,9 +19,9 @@ const Home = () => {
     <>
       <h1>Latest Products</h1>
       {loading ? (
-        <h2>Loading...</h2>
+        <Loader />
       ) : error ? (
-        <h3>{error}</h3>
+        <Message variant={'danger'}>{error}</Message>
       ) : (
         <Row>
           {products.map((product) => (
--- client/src/store.js
+++ client/src/store.js
@@ -1,11 +1,16 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { productListReducer } from './reducers/productReducers';
+import {
+  productDetailsReducer,
+  productListReducer,
+} from './reducers/productReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
+  productDetails: productDetailsReducer,
 });
+
 const initialState = {};
 const middleware = [thunk];
 
