8d1c7826706795493350faa3c05cb10d9188c956
Sakil Khan <sakilk130@gmail.com>
2022-02-07 15:42:59 +0600

    feat(api): production condition

--- api/server.js
+++ api/server.js
@@ -22,10 +22,6 @@ if (process.env.NODE_ENV === 'development') {
 }
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
@@ -36,7 +32,16 @@ app.get('/api/config/paypal', (req, res) => {
 });
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/build')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
 app.use(notFound);
 app.use(errorHandler);
 
